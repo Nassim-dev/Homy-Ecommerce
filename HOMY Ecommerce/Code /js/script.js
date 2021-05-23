301ed068d5015ebdcf6401ba10865dcfcdfb2c41
Nassim YAZI <78474187+Nassim-dev@users.noreply.github.com>
2021-05-23 12:12:04 +0200

    Add files via upload

---
+++ HOMY Ecommerce/Code /js/script.js
@@ -0,0 +1,34 @@
+// Afficher tous les éléments de la suggestions dans la barre de recherche 
+let searchWrapper = document.querySelector(".search-input");
+let inputBox = searchWrapper.querySelector("input");
+let suggestionBox = searchWrapper.querySelector(".suggestion");
+
+// Lorsque l'utilisateur tape les lettres correspondantes de l'objet voulu
+inputBox = (letter)=>{
+    let userData = letter.target.value;
+    let emptyArray = [];
+    if(userData){
+        emptyArray = suggestion.filter((data) => {
+            //Filtrer les mots en fonction des lettres tapé par l'utilisateur
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            return data = '<li>'+ data +'</li>';
+        });
+        console.log(emptyArray);
+        searchWrapper.classList.add("active");
+    } else {
+
+    }
+    showSuggestion(emptyArray);
+}
+
+function showSuggestion(list) {
+    let listData;
+    if(!list.lenght){
+
+    }else{
+        listData = list.join('');
+    }
+    suggestionBox.innerHTML(listData);
+}
