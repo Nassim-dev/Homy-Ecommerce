e5f915dffd7c13914b8b1f552ac34d7ef497eb62
Nassim YAZI <78474187+Nassim-dev@users.noreply.github.com>
2021-05-24 02:20:37 +0200

    Nassim0220

--- Code /js/list-products.js
+++ Code /js/list-products.js
@@ -1,3 +1,52 @@
-function OpenCategory(cat,color) {
-    
+
+
+const listContainer = document.querySelector(".list-item")
+let value = document.querySelector("#mobilier").value
+const nameCat = document.querySelector(".name-category")
+
+let globalMobilier = false
+
+
+window.addEventListener("load", function(e) {
+    console.log("click")
+    nameCat.innerHTML = "hello"
+})
+
+function OpenJSON_Category(cat) {
+    console.log("open")
+    fetch('json.json').then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    }).then(function(json) {
+
+        if (cat == "Botanique"){
+            cat = json.Categories[0].Botanique
+        return cat
+        }
+        if (cat == "Luminaire"){
+            cat = json.Categories[1].Luminaire
+        return cat
+        }
+        if (cat == "Mobilier"){
+            console.log("open MOB")
+            cat = json.Categories[2].Mobilier
+            nameCat.innerHTML = "Mobilier"
+        return cat
+        }
+
+    })
+}
+
+class Products {
+    async getProducts(){
+        try {
+            let json = await fetch('json.json')
+            let data = await json.json()
+             
+        } catch (error) {
+            console.log("Erreur !!")
+        }
+    }
 }
