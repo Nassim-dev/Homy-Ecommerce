6c041581194d8ee9b25487077ea027b5d714aa7d
Nassim YAZI <78474187+Nassim-dev@users.noreply.github.com>
2021-05-25 22:14:26 +0200

    b

--- script/list-products.js
+++ script/list-products.js
@@ -13,16 +13,24 @@ console.log("hello world")
 
 
 
-function getData() {
+function getData(indx) {
     fetch('script/data.json')
     .then(response => {
         return response.json()
     })
-    .then(response=> { resolve (response)})
+    .then(function(json) { 
+        console.log(json)
+        for(var i = 0; i < Object.keys(json.Categories[0])[0].length; i++) {
+        
+            listItem = json.Categories[0].Botanique[i].Nom;
+            console.log(listItem)
+        }
+
+        
+    })
     
 }
 
 const data = getData()
 
-console.log(data)
 
