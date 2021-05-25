cf716a55313236fad28e4df56dc0c4c5b0fb6ace
Nassim YAZI <78474187+Nassim-dev@users.noreply.github.com>
2021-05-25 23:17:08 +0200

    Update list-products.js

--- script/list-products.js
+++ script/list-products.js
@@ -12,25 +12,50 @@ console.log("hello world")
 
 
 
-
 function getData(indx) {
     fetch('script/data.json')
     .then(response => {
         return response.json()
     })
-    .then(function(json) { 
-        console.log(json)
-        for(var i = 0; i < Object.keys(json.Categories[0])[0].length; i++) {
+    .then(function(json) {
+
+        var catName = Object.keys(json.Categories[indx])[0]
+        nameCat.innerHTML = catName
+
+        function listing(Cat) {
+
+            for(var i = 0; i < Object.values(json.Categories[indx])[0].length; i++) {
         
-            listItem = json.Categories[0].Botanique[i].Nom;
-            console.log(listItem)
+                listItem = Object.values(Cat)[i].Nom
+                console.log(listItem)
+            }
+            
+        }
+
+        if (indx == 0) {
+            
+        var cat = json.Categories[0].Botanique
+        const ls = listing(cat)
+    
+        }
+        if (indx == 1) {
+            var cat = json.Categories[1].Luminaire
+            const ls = listing(cat)
+        }
+        if (indx == 2) {
+            var cat = json.Categories[2].Mobilier
+            const ls = listing(cat)
         }
 
         
+
+        
     })
-    
 }
 
-const data = getData()
+
+
+
+const data = getData(2)
 
 
