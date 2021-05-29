c374cfec86d5951dfea491d982781eff3fad02f8
Alexandre CLER <alex28.cler@gmail.com>
2021-05-30 00:24:59 +0200

    Produit

---
+++ js/suggestion.js
@@ -0,0 +1,18 @@
+// Liste des suggestions de la barre de recherche 
+let suggestion = [
+    "Canapés",
+    "Bougies",
+    "Plante",
+    "Tableau",
+    "Scuplture",
+    "Chaises",
+    "Table",
+    "Lampe",
+    "Luminaire",
+    "Bureau",
+    "Fauteuil",
+    "Tapis",
+    "Armoir",
+    "Miroirs",
+    "Peinture",
+];
